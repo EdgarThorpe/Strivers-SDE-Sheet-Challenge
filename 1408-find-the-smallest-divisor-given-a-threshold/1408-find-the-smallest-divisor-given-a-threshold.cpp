1e186bdc4ed41971014e89218e882ea92ce9937b
EdgarThorpe <121433122+EdgarThorpe@users.noreply.github.com>
2023-07-05 23:45:09 +0530

    Time: 54 ms (52.63%), Space: 22.2 MB (86.03%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
@@ -7,18 +7,28 @@ public:
         }
         return result;
     }
+
     
 
-//     int smallestDivisor(vector<int>& nums, int threshold) {
-//         int n = nums.size();
-//         int res = INT_MAX;
-//         int lar = nums[0];
-//         for (int i = 1; i < n; i++) {
-//             if (nums[i] >= lar) {
-//                 lar = nums[i];
-//             }
-//         }
-//         int l = 0, h = lar;
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int n = nums.size();
+        int res = INT_MAX;
+        int lar = nums[0];
+        // for (int i = 1; i < n; i++) {
+        //     if (nums[i] >= lar) {
+        //         lar = nums[i];
+        //     }
+        // }
+        // int maxVal = nums[0];  // Assume the first element is the maximum
+    
+        // for (int num : nums) {
+        //     if (num > maxVal) {
+        //         maxVal = num;  // Update the maximum value
+        //     }
+        // }
+        // int l = 0, h = maxVal;
+
+
 //         int ans = 0;
 //         while (l <= h) {
 //             int mid = h + ((l - h) / 2);
@@ -38,27 +48,25 @@ public:
 
 
     
-    int smallestDivisor(vector<int>& nums, int threshold) {
         int ans = -1;
+        int l = 1;
+        int h = *max_element(nums.begin(), nums.end());
         
-        int low = 1;
-        int high = *max_element(nums.begin(), nums.end());
-        
-        // Iterate using binary search on all divisors.
-        while (low <= high) {
-            int mid = (low + high) / 2;
+
+        while (l <= h) {
+            int mid = (l + h) / 2;
             int result = binSearch(nums, mid);
             // If current divisor does not exceed threshold, 
             // then it can be our answer, but also try smaller divisors
             // thus change search space to left half.
             if (result <= threshold) {
                 ans = mid;
-                high = mid - 1;
+                h = mid - 1;
             }
             // Otherwise, we need a bigger divisor to reduce the result sum
             // thus change search space to right half.
             else {
-                low = mid + 1;
+                l = mid + 1;
             }
         }
         
