3e735f3438c525f256bb6ae2174075523019a67a
EdgarThorpe <121433122+EdgarThorpe@users.noreply.github.com>
2023-07-09 17:26:53 +0530

    Time: 0 ms (100%), Space: 6 MB (38.28%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int numTrees(int n) {
-        int dp[n+1];
-        dp[0]=1;
-        for(int i = 1; i <= n; i++){
-            dp[i] = 0;
-            for(int j = 0; j < i; j++){
-                dp[i]+=dp[j]*dp[i-j-1];
-            }
+        // int dp[n+1];
+        // dp[0]=1;
+        // for(int i = 1; i <= n; i++){
+        //     dp[i] = 0;
+        //     for(int j = 0; j < i; j++){
+        //         dp[i]+=dp[j]*dp[i-j-1];
+        //     }
+        // }
+        // return dp[n];
+        long long C = 1;
+        for (int i = 0; i < n; ++i) {
+            C = C * 2 * (2 * i + 1) / (i + 2);
         }
-        return dp[n];
+        return int (C);
+
     }
 };
