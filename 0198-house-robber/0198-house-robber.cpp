c48b8449c6f66224f534abb70ad72004ecbe6e5f
EdgarThorpe <121433122+EdgarThorpe@users.noreply.github.com>
2023-07-09 18:16:30 +0530

    Time: 2 ms (43.03%), Space: 7.6 MB (95.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         
+        // Space optimized, SC = O(1)
+
         int n = nums.size();
         
         if(n==1) return nums[0];
@@ -15,5 +17,19 @@ public:
             prev = res;
         }
         return res;
+
+
+        // Dp, SC = O(n)
+
+        // int n = nums.size();
+        
+        // if(n==1) return nums[0];
+        // vector<int>dp(n+1);
+        // dp[1]=nums[0];
+        // dp[2]=max(nums[0],nums[1]);
+        // for(int i=3;i<=n;i++){
+        //     dp[i]=max(dp[i-1],dp[i-2]+nums[i-1]);
+        // }
+        // return dp[n];
     }
 };
