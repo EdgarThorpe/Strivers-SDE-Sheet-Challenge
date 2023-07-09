bd262b44e990e526c756b6ceecba1be16f9ea882
EdgarThorpe <121433122+EdgarThorpe@users.noreply.github.com>
2023-07-09 18:15:01 +0530

    Time: 5 ms (28.74%), Space: 7.7 MB (75.92%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,12 +5,15 @@ public:
         int n = nums.size();
         
         if(n==1) return nums[0];
-        vector<int>dp(n+1);
-        dp[1]=nums[0];
-        dp[2]=max(nums[0],nums[1]);
+        // vector<int>dp(n+1);
+        int prev_prev = nums[0];
+        int prev=max(nums[0],nums[1]);
+        int res = prev;
         for(int i=3;i<=n;i++){
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i-1]);
+            res=max(prev,prev_prev+nums[i-1]);
+            prev_prev = prev;
+            prev = res;
         }
-        return dp[n];
+        return res;
     }
 };
