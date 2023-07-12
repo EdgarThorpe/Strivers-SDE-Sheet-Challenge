d3b840f71a1565361b5d87e295cfa78f022f4e2b
EdgarThorpe <121433122+EdgarThorpe@users.noreply.github.com>
2023-07-12 20:56:07 +0530

    Time: 28 ms (97.04%), Space: 10.6 MB (87.66%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,3 +1,29 @@
+// class Solution {
+// public:
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int n = text1.length();
+//         int m = text2.length();
+//         int dp[n+1][m+1];
+//         for(int i = 0; i <= n; i++){
+//             dp[i][0]=0;
+//         }
+//         for(int i = 0; i <= m; i++){
+//             dp[0][i]=0;
+//         }
+//         for(int i = 1; i <= n; i++){
+//             for(int j = 1; j <= m; j++){
+//                 if(text1[n-1]==text2[m-1]){
+//                     dp[i][j] = 1 + dp[i-1][j-1];
+//                 }
+//                 else{
+//                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+//                 }
+//             }
+//         }
+//         return dp[n][m];
+//     }
+// };
+
 class Solution {
 public:
    
