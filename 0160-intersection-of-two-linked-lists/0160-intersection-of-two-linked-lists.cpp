ac7b494089b81f173b1d81cac3c35d4a1d8def6c
EdgarThorpe <121433122+EdgarThorpe@users.noreply.github.com>
2023-08-09 23:45:01 +0530

    Time: 265 ms (10.04%), Space: 14.5 MB (54.88%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,70 +1,70 @@
-class Solution {
-public:
-	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-		// int n = 0;
-		// int m = 0;
-		// ListNode* ptr1 = headA;
-		// ListNode* ptr2 = headB;
-		// while(ptr1 != NULL){
-		// 	n++;
-		// 	ptr1 = ptr1 -> next;
-		// }
-		// while(ptr2 != NULL){
-		// 	m++;
-		// 	ptr2 = ptr2 -> next;
-		// }
-		// int t = abs(n - m);
-		// if(n > m){
-		// 	while(t){
-		// 		headA = headA -> next;
-		// 		t--;
-		// 	}
-		// }
-		// else{
-		// 	while(t){
-		// 		headB = headB -> next;
-		// 		t--;
-		// 	}
-		// }
-		// while(headA != NULL and headB != NULL){
-		// 	if(headA == headB){
-		// 		return headA;
-		// 	}
-		// 	headA = headA -> next;
-		// 	headB = headB -> next;
-		// }
-		// return NULL;
+// class Solution {
+// public:
+// 	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+// 		// int n = 0;
+// 		// int m = 0;
+// 		// ListNode* ptr1 = headA;
+// 		// ListNode* ptr2 = headB;
+// 		// while(ptr1 != NULL){
+// 		// 	n++;
+// 		// 	ptr1 = ptr1 -> next;
+// 		// }
+// 		// while(ptr2 != NULL){
+// 		// 	m++;
+// 		// 	ptr2 = ptr2 -> next;
+// 		// }
+// 		// int t = abs(n - m);
+// 		// if(n > m){
+// 		// 	while(t){
+// 		// 		headA = headA -> next;
+// 		// 		t--;
+// 		// 	}
+// 		// }
+// 		// else{
+// 		// 	while(t){
+// 		// 		headB = headB -> next;
+// 		// 		t--;
+// 		// 	}
+// 		// }
+// 		// while(headA != NULL and headB != NULL){
+// 		// 	if(headA == headB){
+// 		// 		return headA;
+// 		// 	}
+// 		// 	headA = headA -> next;
+// 		// 	headB = headB -> next;
+// 		// }
+// 		// return NULL;
 
-		if(headA==NULL || headB == NULL){
-			return NULL;
-		}
+// 		if(headA==NULL || headB == NULL){
+// 			return NULL;
+// 		}
 
 
-		int countA = 0;
-		for(ListNode* currA = headA; currA->next!=NULL; currA=currA->next){
-			countA++;
-		}
-		int countB = 0;
-		for(ListNode* currB = headB; currB->next!=NULL; currB=currB->next){
-			countB++;
-		}
+// 		int countA = 0;
+// 		for(ListNode* currA = headA; currA->next!=NULL; currA=currA->next){
+// 			countA++;
+// 		}
+// 		int countB = 0;
+// 		for(ListNode* currB = headB; currB->next!=NULL; currB=currB->next){
+// 			countB++;
+// 		}
 
-		int diff = abs(countA-countB);
-		if(countA > countB){
-			while(diff--){
-				headA=headA->next;
-			}
-		}
-		else if(countB > countA){
-			while(diff--){
-				headB=headB->next;
-			}
-		}
-		while(headA!=headB){
-				headA=headA->next;
-				headB=headB->next;
-		}
-		return headA;
+// 		int diff = abs(countA-countB);
+// 		if(countA > countB){
+// 			while(diff--){
+// 				headA=headA->next;
+// 			}
+// 		}
+// 		else if(countB > countA){
+// 			while(diff--){
+// 				headB=headB->next;
+// 			}
+// 		}
+// 		while(headA!=headB){
+// 				headA=headA->next;
+// 				headB=headB->next;
+// 		}
+// 		return headA;
 
 
 
@@ -74,17 +74,43 @@ public:
 
 
 		
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        // ListNode *pA = headA;
+        // ListNode *pB = headB;
+        // while (pA != pB) {
+        //     pA = pA == nullptr ? headB : pA->next;
+        //     pB = pB == nullptr ? headA : pB->next;
+        // }
+        // return pA;
 
+        ListNode* pA = headA;
+        ListNode* pB = headB;
 
+        while(pA!=pB){
+            pA = pA == NULL ? headA: pA->next;
+            pB = pB == NULL ? headB: pB->next;
 
+        }
+        return pA;
 
-
-
-
-
-
-
-
-
-	}
+        // Note: In the case lists do not intersect, the pointers for A and B
+        // will still line up in the 2nd iteration, just that here won't be
+        // a common node down the list and both will reach their respective ends
+        // at the same time. So pA will be NULL in that case.
+    }
 };
+
+
+
+
+
+
+
+
+
+
+
+// 	}
+// };
