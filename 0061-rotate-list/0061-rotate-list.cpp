f49c6d30464fb4483d30290333a2e8ad5e13cbcd
EdgarThorpe <121433122+EdgarThorpe@users.noreply.github.com>
2023-08-10 19:40:10 +0530

    Time: 5 ms (77.87%), Space: 11.7 MB (59.62%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,31 +11,57 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if (head == NULL || head->next == NULL || k == 0) {
-            return head;
-        }
+        // if (head == NULL || head->next == NULL || k == 0) {
+        //     return head;
+        // }
         
+        // int len = 1;
+        // ListNode* tail = head;
+        // while (tail->next != NULL) {
+        //     tail = tail->next;
+        //     len++;
+        // }
+        
+        // k = k % len;
+        // if (k == 0) {
+        //     return head;
+        // }
+        
+        // ListNode* curr = head;
+        // for (int i = 1; i < len - k; i++) {
+        //     curr = curr->next;
+        // }
+        
+        // tail->next = head;
+        // head = curr->next;
+        // curr->next = NULL;
+        
+        // return head;
+
+        if(head==NULL) return NULL;
+
         int len = 1;
         ListNode* tail = head;
-        while (tail->next != NULL) {
-            tail = tail->next;
+        while(tail->next!=NULL){
+            tail=tail->next;
             len++;
         }
-        
-        k = k % len;
-        if (k == 0) {
-            return head;
+
+        k %= len;
+
+        if(k==0) return head;
+
+        ListNode* temp = head;
+        for(int i = 1; i < len-k; i++){
+            temp=temp->next;
         }
-        
-        ListNode* curr = head;
-        for (int i = 1; i < len - k; i++) {
-            curr = curr->next;
-        }
-        
         tail->next = head;
-        head = curr->next;
-        curr->next = NULL;
-        
+        head=temp->next;
+        temp->next = NULL;
         return head;
+
+
+
+
     }
 };
