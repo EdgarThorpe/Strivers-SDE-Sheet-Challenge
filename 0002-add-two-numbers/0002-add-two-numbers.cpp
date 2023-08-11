604a28bac0f4d683bca0932c7951bbb7cc0447c0
EdgarThorpe <121433122+EdgarThorpe@users.noreply.github.com>
2023-08-11 13:39:23 +0530

    Time: 41 ms (39.4%), Space: 71.4 MB (68.32%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,46 +10,67 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int sum=(l1->val+l2->val);
-        ListNode* head3= new ListNode(sum%10);
-        int carry=sum/10;
-        ListNode* head1=l1;
-        ListNode* head2=l2;
+    ListNode* addTwoNumbers(ListNode* first, ListNode* second) {
+        ListNode* head1 = first;
+        ListNode* head2 = second;
+        int sum = (first->val+second->val);
         head1=head1->next;
         head2=head2->next;
-        ListNode* ret=head3;
-        while(head1 || head2)
-        {
-            int l=carry;
-            if(head1)
-            {
+
+        int carry = sum/10;
+
+        ListNode* head3 = new ListNode(sum%10);
+        ListNode* ret = head3;
+
+        while(head1 || head2){
+            int l = carry;
+            if(head1){
                 l+=head1->val;
                 head1=head1->next;
             }
-            if(head2)
-            {
+            if(head2){
                 l+=head2->val;
                 head2=head2->next;
             }
-            ListNode* y=new ListNode(l%10);
-            head3->next=y;
-            head3=y;
+            ListNode* y = new ListNode(l%10);
             carry=l/10;
-            // if(head1)
-            // {
-            // }
-            // if(head2)
-            // {
-            // }
+            head3->next = y;
+            head3 = head3->next;
+        }
+        if(carry){
+            ListNode* carryNode = new ListNode(carry);
+            head3->next = carryNode;
+        }
 
-        }
-        if(carry)
-        {
-            ListNode* last=new ListNode(carry);
-            head3->next=last;
-        }
         return ret;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
     }
 };
