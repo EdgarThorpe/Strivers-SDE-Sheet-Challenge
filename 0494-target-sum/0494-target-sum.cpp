2c3e3480fa26fd76750b2b5152afdd5bf9cc4727
EdgarThorpe <121433122+EdgarThorpe@users.noreply.github.com>
2023-08-28 22:25:48 +0530

    Time: 4 ms (97.63%), Space: 9.1 MB (67.96%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,33 +1,3 @@
-// class Solution {
-// public:
-//     int findTargetSumWays(vector<int>& nums, int target) {
-        
-//     }
-// };
-
-
-// 	public:
-// 	int subsetCount(int arr[], int n, int sum)
-// 	{
-//        int t[n+1][sum+1];
-//        for(int i=1;i<sum+1;i++)
-//             t[0][i] = 0;
-       
-//        for(int i=0;i<n+1;i++)
-//             t[i][0] = 1;
-        
-//         for(int i=1;i<n+1;i++)
-//             for(int j=1;j<sum+1;j++){
-//                 if(arr[i-1] <= j)
-//                     t[i][j] = (t[i-1][j] + t[i-1][j-arr[i-1]]);
-//                 else
-//                     t[i][j] = t[i-1][j];
-//             }
-            
-//         return t[n][sum];
-// 	}
-	  
-// };
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
@@ -37,8 +7,8 @@ public:
         if (abs(target) > sum || (sum + target) % 2 != 0){ 
             return 0;
         }
-        int s1 = (sum + target)/2;
-        int ans = subsetCount(nums,s1);
+        int s1 = (sum + target);
+        int ans = subsetCount(nums,s1/2);
         return ans;
     }
     
@@ -50,10 +20,10 @@ public:
             t[0][i] = 0;
         for(int i=1;i<n+1;i++)
             for(int j=0;j<s1+1;j++){
-                if(nums[i-1] > j)
-                    t[i][j] = t[i-1][j];
-                else
+                if(nums[i-1] <= j)
                     t[i][j] = t[i-1][j] + t[i-1][j-nums[i-1]];
+                else
+                    t[i][j] = t[i-1][j];
             }
         
         return t[n][s1];
